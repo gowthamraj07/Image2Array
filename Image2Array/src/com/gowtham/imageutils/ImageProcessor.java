62ee45fe06f99784d801b9ce7c77fbeba9a183d3
gowtham <gowtham@Student>
2016-12-12 13:58:20 +0530

    Initial Commit

---
+++ Image2Array/src/com/gowtham/imageutils/ImageProcessor.java
@@ -0,0 +1,107 @@
+package com.gowtham.imageutils;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+
+public class ImageProcessor {
+
+	public static void main(String[] args) {
+		try {
+			BufferedImage img = ImageIO.read(new File("16x16/images.png"));
+			/*byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+			for(byte b : pixels) {
+				System.out.println(b);
+			}*/
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy MM dd hh:mm:ss");
+			System.out.println("Starting at : "+sdf.format(new Date()));
+			int[][] res = convertTo2DWithoutUsingGetRGB(img);
+			System.out.println("Ending at   : "+sdf.format(new Date()));
+			System.out.println("Size : "+res.length+"x"+res[0].length);
+			for(int i=0;i<res.length;i++) {
+				for(int j=0;j<res[i].length;j++) {
+					System.out.print(""+res[i][j]+" ");
+				}
+				System.out.println();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static int[][] getBWImage(BufferedImage img) {
+		int[][] res = convertTo2DWithoutUsingGetRGB(img);
+		int[][] byteRes = new int[res.length][res[0].length];
+		
+		for(int i=0;i<res.length;i++){
+  		  for(int j=0; j<res[i].length;j++) {
+  			  
+  			  byteRes[i][j] =  (res[i][j] >= -14000000 ? 1 : 0);
+  		  }
+  		  //System.out.println();
+  	  }
+		
+		return byteRes;
+	}
+	
+	public static int[][] convertTo2DWithoutUsingGetRGB(BufferedImage image) {
+
+	      final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+	      final int width = image.getWidth();
+	      final int height = image.getHeight();
+	      final boolean hasAlphaChannel = image.getAlphaRaster() != null;
+	      System.out.println("Type : "+image.getType());
+
+	      int[][] result = new int[height][width];
+	      if (hasAlphaChannel) {
+	         final int pixelLength = 4;
+	         for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+	            int argb = 0;
+	            argb += (((int) pixels[pixel] & 0xff) << 24); // alpha
+	            argb += ((int) pixels[pixel + 1] & 0xff); // blue
+	            argb += (((int) pixels[pixel + 2] & 0xff) << 8); // green
+	            argb += (((int) pixels[pixel + 3] & 0xff) << 16); // red
+	            result[row][col] = argb;
+	            col++;
+	            if (col == width) {
+	               col = 0;
+	               row++;
+	            }
+	         }
+	      } else if(image.getType() == 13) {
+	    	  for(int i=0;i<image.getHeight();i++){
+	    		  for(int j=0; j<image.getWidth();j++) {
+	    			  int rgb = image.getRGB(i, j);
+	    			  //System.out.print(rgb+"\t");
+	    			  result[i][j] = rgb; // >= -16000000 ? 1 : 0
+	    		  }
+	    		  //System.out.println();
+	    	  }
+	      } else {
+	         final int pixelLength = 3;
+	         for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+	            int argb = 0;
+	            argb += -16777216; // 255 alpha
+	            argb += ((int) pixels[pixel] & 0xff); // blue
+	            argb += (((int) pixels[pixel + 1] & 0xff) << 8); // green
+	            argb += (((int) pixels[pixel + 2] & 0xff) << 16); // red
+	            result[row][col] = argb;
+	            col++;
+	            if (col == width) {
+	               col = 0;
+	               row++;
+	            }
+	         }
+	      }
+
+	      return result;
+	   }
+
+}
---
+++ Image2Array/src/com/gowtham/imageutils/Test.java
@@ -0,0 +1,14 @@
+package com.gowtham.imageutils;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 8;
+		System.out.println(i);
+		System.out.println(""+(byte)i);
+		System.out.println(""+Integer.valueOf(i).byteValue());
+
+	}
+
+}
